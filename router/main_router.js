5cbac5828c7972ddde793285486ef7dec53f023c
jemblonganvalley <jemblonganvalley@gmail.com>
2020-12-22 15:30:48 +0700

    update landing

--- router/main_router.js
+++ router/main_router.js
@@ -16,6 +16,7 @@ router.get("/", (req, res) => {
       width : 100vw;
       height : 100vh;
       display : flex;
+      flex-direction : column;
       justify-content : center;
       align-items : center;
     ">
